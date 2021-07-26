6d576e819cfdf258c1aa218e05e0a1773c87cedf
shao-yuqian <1423093239@qq.com>
2021-07-26 17:50:51 +0800

    短信

--- 03express/短信验证码登录/server.js
+++ 03express/短信验证码登录/server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const app = express();
+const route = express.Router();
+app.listen(3000);
+
+// 获取短信验证码接口
+// 1.服务器3000 向阿里云发送请求
+// 发送短信的内容
+// 短信的模板
+// 
+route.get('/vertifyCode',function(req,res){
+    
+})
+
+app.route();
